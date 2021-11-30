f08ce915fdd479dfb213f1895c73aee0c32d7e9a
Erico Stuurman <info@ericoonline.nl>
2021-11-30 15:41:50 +0100

    added react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-
+import React from "react"
+import { Routes, Route } from "react-router";
 import './App.css';
+import ChartAvg from "./components/ChartAvg";
+
 
 function App() {
+
+
+  
   return (
     <div className="App">
-      
+      <Routes>
+      <Route path="/" element={<ChartAvg/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/ChartAvg.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+const ChartAvg = () =>{
+    return (
+      <h1>Here comes the Victory chart </h1>  
+    )
+    
+
+}
+
+export default ChartAvg
